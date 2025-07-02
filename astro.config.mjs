020a39e13ed71e42102a2d0af2583818364fa655
= <=>
2025-07-02 21:11:31 +0900

    Astroプロジェクトの初期設定を更新し、サイトURL、ベースパス、出力形式を設定。新たに開発依存関係としてvite-plugin-mkcertを追加。index.astroファイルをARデモ用に更新。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://tarabakz25.github.io',   // ← 必ず https:// で!
+  base: '/webar-astro',                         // ← プロジェクトページなら必須
+  output: 'static',                        // ← 既定値だが明示しておく
+});
